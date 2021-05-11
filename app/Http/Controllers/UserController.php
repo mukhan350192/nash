2b397b084757064f8d6b35e0c8a4bb600093625e
mukhan350192 <mukhan350129@gmail.com>
2021-05-11 20:03:02 +0600

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -137,6 +137,7 @@ class UserController extends Controller
                     ]
                 ]);
                 $response = $response->getBody()->getContents();
+                var_dump($response);
                 $response = json_decode($response, true);
                 if ($response['success'] == true) {
                     $result['success'] = true;
